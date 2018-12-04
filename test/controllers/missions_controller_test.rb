345304b96a12f1f6ffe3e28a84bc07c25beab5f7
Tim <timothee.bull@gmail.com>
2018-12-04 13:52:27 +0100

    ajouté les test de controllers et de route de base

--- test/controllers/missions_controller_test.rb
+++ test/controllers/missions_controller_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 
 class MissionsControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get missions_index_url
-  #   assert_response :success
-  # end
+  test "route de base missions/index accessible" do
+    get missions_index_url
+    assert_response :success
+  end
 
-  # test "should get show" do
-  #   get missions_show_url
-  #   assert_response :success
-  # end
+  test "route de base missions/show accessible" do
+    get missions_show_url
+    assert_response :success
+  end
 
-  # test "should get edit" do
-  #   get missions_edit_url
-  #   assert_response :success
-  # end
+  test "route de base missions/edit accessible" do
+    get missions_edit_url
+    assert_response :success
+  end
 
 end
