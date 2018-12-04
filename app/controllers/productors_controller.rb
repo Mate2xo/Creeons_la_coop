4554b8b793dc1037a26d6afa14d167cf4119603f
jerome.delfosse <jerome.delfosse.78@gmail.com>
2018-12-04 19:53:06 +0100

    ajout de g map

--- app/controllers/productors_controller.rb
+++ app/controllers/productors_controller.rb
@@ -8,7 +8,7 @@ class ProductorsController < ApplicationController
 		@valide = "addresse invalide"
 		url = "7 rue de la haute pierre 78620"
 		if (@address = Addressable::URI.parse(url).normalize.to_str) != ""
-		response = RestClient.get (("https://maps.googleapis.com/maps/api/geocode/json?address=" + @address + "+france&key=AIzaSyCzGvZfemye0TE-2dCRM7VNXhSzms-PqCw")), {accept: :json}
+		response = RestClient.get ("https://maps.googleapis.com/maps/api/geocode/json?address=" + @address + "+france&key=" + Rails.application.credentials[:google_key] ), {accept: :json}
 		response = JSON.parse(response.body)
 		if response["results"][0]
 			@lat = response["results"][0]["geometry"]["location"]["lat"]
