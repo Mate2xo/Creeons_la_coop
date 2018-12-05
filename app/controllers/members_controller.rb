a0c272b4234c8b44bf89c7de2d28efbcb71c642a
Varuamana <corentin.nadaud@orange.fr>
2018-12-05 16:02:41 +0100

    controler

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,10 +1,12 @@
 class MembersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @member = Member.all
   end
 
   def show
+    @member = Member.find(params[:id])
   end
 
-  def edit
-  end
 end
