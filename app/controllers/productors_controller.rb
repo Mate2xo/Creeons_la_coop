c1bf642330223d75c9e55f4e464b1f5daeceb1fd
Tim <timothee.bull@gmail.com>
2018-12-08 11:20:02 +0100

    indentation dans productors_controller.rb

--- app/controllers/productors_controller.rb
+++ app/controllers/productors_controller.rb
@@ -13,8 +13,8 @@ class ProductorsController < ApplicationController
 		@valide = "addresse invalide"
 		url = "7 rue de la haute pierre 78620"
 		if (@address = Addressable::URI.parse(url).normalize.to_str) != (nil || "")
-		response = RestClient.get ("https://maps.googleapis.com/maps/api/geocode/json?address=" + @address + "+france&key=" + Rails.application.credentials[:google_key] ), {accept: :json}
-		response = JSON.parse(response.body)
+			response = RestClient.get ("https://maps.googleapis.com/maps/api/geocode/json?address=" + @address + "+france&key=" + Rails.application.credentials[:google_key] ), {accept: :json}
+			response = JSON.parse(response.body)
 			if response["status"] == "OK" && response["results"][0]
 				@lat = response["results"][0]["geometry"]["location"]["lat"]
 				@lng = response["results"][0]["geometry"]["location"]["lng"]
