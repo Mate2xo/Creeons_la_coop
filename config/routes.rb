542bf6c2111333a870d079a6b4191f92095c3ab2
Janus7593 <soparisbandit@gmail.com>
2018-12-12 18:10:55 +0100

    Page Shop Ok + details

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :members
   root 'static_pages#home'
   get 'dashboard', to: "static_pages#dashboard"
+  get 'shop', to: "static_pages#shop"
 
   get 'admin', to: "admin#show"
   post 'admin/delete/:class/:id', to: "admin#destroy"
