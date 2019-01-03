02c335783dc24f07da30ab188f9e27318cef1fdf
Tim <mate2xo@gmail.com>
2019-01-03 14:15:32 +0000

    ajout helpers admin? et super_admin?, ajout variables dans show admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def super_admin?
+    member_signed_in? && current_member.role == "super_admin"
+  end
+  
+  def admin?
+    member_signed_in? && current_member.role == "admin"
+  end
+  
   protected
 
   def configure_permitted_parameters
