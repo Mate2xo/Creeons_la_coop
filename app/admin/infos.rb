f5e2c2e00df0ef069c6fca7ec0ce0297af853315
Tim <timothee.bull@gmail.com>
2019-02-17 19:06:18 +0000

    basic admin/infos controller tests ok

--- app/admin/infos.rb
+++ app/admin/infos.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Info do
+  permit_params :content, :title, :author_id
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
--- app/admin/members.rb
+++ app/admin/members.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Member do
-  permit_params :email
+  permit_params :email, :encrypted_password, :first_name, :last_name, :biography, :phone_number, :role
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
