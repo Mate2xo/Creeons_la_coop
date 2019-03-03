23877a3c9bcc71bddef739ad43b3c0c6ed05165e
Tim <timothee.bull@gmail.com>
2019-03-03 18:25:33 +0000

    simplefied admin/productor layout

--- app/admin/productors.rb
+++ app/admin/productors.rb
@@ -2,4 +2,15 @@
 
 ActiveAdmin.register Productor do
   permit_params :name, :description, :phone_number, :website_url
+
+  index do
+    selectable_column
+    column :name
+    column :description
+    column :phone_number
+    column :website_url do |productor|
+      link_to productor.website_url, productor.website_url
+    end
+    actions
+  end
 end
