0e2fb56007f633ba8259ef2699cf14775a6d945a
Tim <timothee.bull@gmail.com>
2019-03-03 16:06:46 +0000

    admin/missions_controller_spec full CRUD tests

--- app/admin/missions.rb
+++ app/admin/missions.rb
@@ -1,17 +1,5 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Mission do
-  permit_params :name, :description, "due_date(1i)", "due_date(2i)", "due_date(3i)", "due_date(4i)", "due_date(5i)"
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :author_id, :name, :description, :due_date
 end
--- spec/controllers/admin/missions_controller_spec.rb
+++ spec/controllers/admin/missions_controller_spec.rb
@@ -3,12 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Admin::MissionsController, type: :controller do
+  render_views
+  let(:page) { Capybara::Node::Simple.new(response.body) }
   let(:super_admin) { create(:member, :super_admin) }
   before { sign_in super_admin }
 
   let!(:mission) { create(:mission) }
 
-  let(:valid_attributes) { attributes_for(:mission) }
+  let(:valid_attributes) { build(:mission).attributes }
 
   let(:invalid_attributes) do
     { name: '' }
@@ -23,12 +25,12 @@ RSpec.describe Admin::MissionsController, type: :controller do
       get :index
       expect(assigns(:missions)).to include(mission)
     end
-    # it "should render the expected columns" do
-    #   get :index
-    #   expect(page).to have_content(person.first_name)
-    #   expect(page).to have_content(person.last_name)
-    #   expect(page).to have_content(person.email)
-    # end
+    it "should render the expected columns" do
+      get :index
+      expect(page).to have_content(mission.name)
+      expect(page).to have_content(mission.description)
+      expect(page).to have_content(mission.due_date.to_s(:long))
+    end
     # let(:filters_sidebar) { page.find('#filters_sidebar_section') }
     # it "filter Name exists" do
     #   get :index
@@ -55,140 +57,140 @@ RSpec.describe Admin::MissionsController, type: :controller do
       get :new
       expect(assigns(:mission)).to be_a_new(Mission)
     end
-    #   it "should render the form elements" do
-    #     get :new
-    #     expect(page).to have_field('First name')
-    #     expect(page).to have_field('Last name')
-    #     expect(page).to have_field('Email')
-    #   end
+    it "should render the form elements" do
+      get :new
+      expect(page).to have_field('Name')
+      expect(page).to have_field('Description')
+      expect(page).to have_field('Due date')
+    end
   end
 
-  # describe "POST create" do
-  #   context "with valid params" do
-  #     it "creates a new Mission" do
-  #       expect {
-  #         post :create, params: { mission: valid_attributes }
-  #       }.to change(Mission, :count).by(1)
-  #     end
+  describe "POST create" do
+    context "with valid params" do
+      it "creates a new Mission" do
+        expect {
+          post :create, params: { mission: valid_attributes }
+        }.to change(Mission, :count).by(1)
+      end
 
-  #     it "assigns a newly created person as @person" do
-  #       post :create, params: { person: valid_attributes }
-  #       expect(assigns(:person)).to be_a(Person)
-  #       expect(assigns(:person)).to be_persisted
-  #     end
+      it "assigns a newly created mission as @mission" do
+        post :create, params: { mission: valid_attributes }
+        expect(assigns(:mission)).to be_a(Mission)
+        expect(assigns(:mission)).to be_persisted
+      end
 
-  #     it "redirects to the created person" do
-  #       post :create, params: { person: valid_attributes }
-  #       expect(response).to have_http_status(:redirect)
-  #       expect(response).to redirect_to(admin_person_path(Person.last))
-  #     end
+      it "redirects to the created mission" do
+        post :create, params: { mission: valid_attributes }
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(admin_mission_path(Mission.last))
+      end
 
-  #     it 'should create the person' do
-  #       post :create, params: { person: valid_attributes }
-  #       person = Person.last
+      it 'should create the mission' do
+        post :create, params: { mission: valid_attributes }
+        mission = Mission.last
 
-  #       expect(person.first_name).to eq(valid_attributes[:first_name])
-  #       expect(person.last_name).to  eq(valid_attributes[:last_name])
-  #       expect(person.email).to      eq(valid_attributes[:email])
-  #     end
-  # end
+        expect(mission.name).to eq(valid_attributes["name"])
+        expect(mission.description).to eq(valid_attributes["description"])
+        expect(mission.due_date).to      eq(valid_attributes["due_date"])
+      end
+    end
 
-  #   context "with invalid params" do
-  #     it 'invalid_attributes return http success' do
-  #       post :create, params: { person: invalid_attributes }
-  #       expect(response).to have_http_status(:success)
-  #     end
+    context "with invalid params" do
+      it 'invalid_attributes return http success' do
+        post :create, params: { mission: invalid_attributes }
+        expect(response).to have_http_status(:success)
+      end
 
-  #     it "assigns a newly created but unsaved person as @person" do
-  #       post :create, params: { person: invalid_attributes }
-  #       expect(assigns(:person)).to be_a_new(Person)
-  #     end
+      it "assigns a newly created but unsaved mission as @mission" do
+        post :create, params: { mission: invalid_attributes }
+        expect(assigns(:mission)).to be_a_new(Mission)
+      end
 
-  #     it 'invalid_attributes do not create a Person' do
-  #       expect do
-  #         post :create, params: { person: invalid_attributes }
-  #       end.not_to change(Person, :count)
-  #     end
-  #   end
-  # end
+      it 'invalid_attributes do not create a Mission' do
+        expect do
+          post :create, params: { mission: invalid_attributes }
+        end.not_to change(Mission, :count)
+      end
+    end
+  end
 
-  # describe "GET edit" do
-  #   before do
-  #     get :edit, params: { id: person.id }
-  #   end
-  #   it 'returns http success' do
-  #     expect(response).to have_http_status(:success)
-  #   end
-  #   it 'assigns the person' do
-  #     expect(assigns(:person)).to eq(person)
-  #   end
-  #   it "should render the form elements" do
-  #     expect(page).to have_field('First name', with: person.first_name)
-  #     expect(page).to have_field('Last name', with: person.last_name)
-  #     expect(page).to have_field('Email', with: person.email)
-  #   end
-  # end
+  describe "GET edit" do
+    before do
+      get :edit, params: { id: mission.id }
+    end
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'assigns the mission' do
+      expect(assigns(:mission)).to eq(mission)
+    end
+    it "should render the form elements" do
+      expect(page).to have_field('Name', with: mission.name)
+      expect(page).to have_field('Description', with: mission.description)
+      expect(page).to have_select('Due date')
+    end
+  end
 
-  # describe "PUT update" do
-  #   context 'with valid params' do
-  #     before do
-  #       put :update, params: { id: person.id, person: valid_attributes }
-  #     end
-  #     it 'assigns the person' do
-  #       expect(assigns(:person)).to eq(person)
-  #     end
-  #     it 'returns http redirect' do
-  #       expect(response).to have_http_status(:redirect)
-  #       expect(response).to redirect_to(admin_person_path(person))
-  #     end
-  #     it "should update the person" do
-  #       person.reload
+  describe "PUT update" do
+    context 'with valid params' do
+      before do
+        put :update, params: { id: mission.id, mission: valid_attributes }
+      end
+      it 'assigns the mission' do
+        expect(assigns(:mission)).to eq(mission)
+      end
+      it 'returns http redirect' do
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(admin_mission_path(mission))
+      end
+      it "should update the mission" do
+        mission.reload
 
-  #       expect(person.last_name).to  eq(valid_attributes[:last_name])
-  #       expect(person.first_name).to eq(valid_attributes[:first_name])
-  #       expect(person.email).to      eq(valid_attributes[:email])
-  #     end
-  #   end
-  #   context 'with invalid params' do
-  #     it 'returns http success' do
-  #       put :update, params: { id: person.id, person: invalid_attributes }
-  #       expect(response).to have_http_status(:success)
-  #     end
-  #     it 'does not change person' do
-  #       expect do
-  #         put :update, params: { id: person.id, person: invalid_attributes }
-  #       end.not_to change { person.reload.first_name }
-  #     end
-  #   end
-  # end
+        expect(mission.name).to eq(valid_attributes["name"])
+        expect(mission.description).to eq(valid_attributes["description"])
+        expect(mission.due_date).to eq(valid_attributes["due_date"])
+      end
+    end
+    context 'with invalid params' do
+      it 'returns http success' do
+        put :update, params: { id: mission.id, mission: invalid_attributes }
+        expect(response).to have_http_status(:success)
+      end
+      it 'does not change mission' do
+        expect do
+          put :update, params: { id: mission.id, mission: invalid_attributes }
+        end.not_to change(mission.reload.name, :methods)
+      end
+    end
+  end
 
-  # describe "GET show" do
-  #   before do
-  #     get :show, params: { id: person.id }
-  #   end
-  #   it 'returns http success' do
-  #     expect(response).to have_http_status(:success)
-  #   end
-  #   it 'assigns the person' do
-  #     expect(assigns(:person)).to eq(person)
-  #   end
-  #   it "should render the form elements" do
-  #     expect(page).to have_content(person.last_name)
-  #     expect(page).to have_content(person.first_name)
-  #     expect(page).to have_content(person.email)
-  #   end
-  # end
+  describe "GET show" do
+    before do
+      get :show, params: { id: mission.id }
+    end
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'assigns the mission' do
+      expect(assigns(:mission)).to eq(mission)
+    end
+    it "should render the form elements" do
+      expect(page).to have_content(mission.name)
+      expect(page).to have_content(mission.description)
+      expect(page).to have_content(mission.due_date.to_s(:long))
+    end
+  end
 
-  # describe "DELETE #destroy" do
-  #   it "destroys the requested select_option" do
-  #     expect {
-  #       delete :destroy, params: { id: person.id }
-  #     }.to change(Person, :count).by(-1)
-  #   end
+  describe "DELETE #destroy" do
+    it "destroys the requested select_option" do
+      expect {
+        delete :destroy, params: { id: mission.id }
+      }.to change(Mission, :count).by(-1)
+    end
 
-  #   it "redirects to the field" do
-  #     delete :destroy, params: { id: person.id }
-  #     expect(response).to redirect_to(admin_persons_path)
-  #   end
-  # end
+    it "redirects to the field" do
+      delete :destroy, params: { id: mission.id }
+      expect(response).to redirect_to(admin_missions_path)
+    end
+  end
 end
