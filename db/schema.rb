501bd9ae8029444eeb71cfa7886dec144f700df4
Yin Yué <yyue87@gmail.com>
2019-03-08 15:29:30 +0100

    add regular attribute to Missions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_184129) do
+ActiveRecord::Schema.define(version: 2019_03_08_142016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 2019_02_11_184129) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
+    t.boolean "regular"
     t.index ["author_id"], name: "index_missions_on_author_id"
   end
 
