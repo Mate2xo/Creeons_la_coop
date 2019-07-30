6fabd2580a9b0bf68308a2c99955c9e3a3efc76e
Tim <timothee.bull@gmail.com>
2019-07-30 06:24:36 +0200

    added a few tests

--- spec/features/member_invitations_spec.rb
+++ spec/features/member_invitations_spec.rb
@@ -9,12 +9,26 @@ RSpec.feature "MemberInvitations", type: :feature do
   }
 
   context "when using the invitation form" do
-    it 'sends an invitation email' do
+    before {
       visit 'members/invitation/new'
       fill_in 'Email', with: 'test@test.com'
       click_button 'Send an invitation'
+    }
+
+    it 'sends an invitation email' do
       expect(Devise.mailer.deliveries.count).to eq 1
       expect(page).to have_content "An invitation email has been sent"
     end
+
+    it 'creates an unvalidated user' do
+      invalid_user = Member.last
+      expect(Member.count).to eq 2
+      expect(invalid_user.email).to eq 'test@test.com'
+      expect(invalid_user.first_name).to eq nil
+      expect(invalid_user.last_name).to eq nil
+    end
+  end
+
+  context "when following the invitation mail's link" do
   end
 end
