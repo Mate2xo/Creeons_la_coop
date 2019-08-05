6a0f791d9e99778cd796fa5b0c8afec8a51bbf37
Tim <timothee.bull@gmail.com>
2019-08-05 12:23:56 +0200

    extrated full_calendar and recurring_select js imports, from application.js to mission.js. Bundled full_calendar for easy requiring

--- app/assets/javascripts/missions.js
+++ app/assets/javascripts/missions.js
@@ -1,3 +1,7 @@
+//= require full_calendar
+//= require recurring_select
+//= require recurring_select/fr
+
 $(document).on('turbolinks:load', function() {
   $("#mission_recurrent").on('change', function() {
     $("#recurrence-selector").toggle(150);
---
+++ vendor/assets/js/full_calendar/index.js
@@ -0,0 +1,5 @@
+//= require full_calendar/core/main.min
+//= require full_calendar/core/locale/fr.js
+//= require full_calendar/daygrid/main.min
+//= require full_calendar/timegrid/main.min
+//= require full_calendar/bootstrap/main.min
