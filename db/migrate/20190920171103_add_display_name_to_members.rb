1ca052ba70a01a815d57eb382f00dbe2acf5b0bd
Tim <timothee.bull@gmail.com>
2019-10-04 15:16:53 +0200

    update add_index_to_display_name migration to properly load Members table information
    Without force-reloading the table, the migrations would pause because :display_name was not found. Relauching the migration was ok though. With this change, all migrations are done in one go

--- db/migrate/20190920171103_add_display_name_to_members.rb
+++ db/migrate/20190920171103_add_display_name_to_members.rb
@@ -3,18 +3,5 @@
 class AddDisplayNameToMembers < ActiveRecord::Migration[5.2]
   def change
     add_column :members, :display_name, :string
-
-    reversible do |dir|
-      dir.up do
-        DbTextSearch::CaseInsensitive.add_index(
-          connection, Thredded.user_class.table_name, Thredded.user_name_column, unique: true
-        )
-      end
-      dir.down do
-        remove_index Thredded.user_class.table_name, name: "#{Thredded.user_class.table_name}_#{Thredded.user_name_column}_lower"
-      end
-    end
-
-    Member.all.each(&:save)
   end
 end
