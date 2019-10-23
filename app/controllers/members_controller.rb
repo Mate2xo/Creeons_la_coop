c3e0953640da122dee0dce0b8b533edccfaaeab0
Tim <timothee.bull@gmail.com>
2019-10-23 11:38:33 +0200

    rename callback to be more explicit

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,7 +3,7 @@
 # The websites users. Their 'role' attributes determines if fhey're an unvalidated user, a member, admin or super-admmin
 class MembersController < ApplicationController
   before_action :authenticate_member!
-  before_action :set_member, only: %i[show edit update]
+  before_action :set_authorized_member, only: %i[show edit update]
 
   def index
     @members = Member.includes(:address, :avatar_attachment)
@@ -31,7 +31,7 @@ class MembersController < ApplicationController
     params.require(:member).permit(:email, :first_name, :last_name, :group, :avatar, :phone_number, :biography, address_attributes: %i[postal_code city street_name_1 street_name_2 coordinates])
   end
 
-  def set_member
+  def set_authorized_member
     @member = authorize Member.find(params[:id])
   end
 end
