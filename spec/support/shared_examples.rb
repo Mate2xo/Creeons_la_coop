e7156ad75219ce1738fadea1c759736a81f80a6e
Tim <timothee.bull@gmail.com>
2019-11-13 18:56:29 +0100

    i18nized productors/index.html
    and delete useless :en locale keys check on views' shared example

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples "a view without missing translations" do
-  describe ":en locale" do
-    it "has no missing keys" do
-      I18n.locale = :en
-      expect{ render }.not_to raise_error
-    end
-  end
-
   describe ":fr locale" do
     it "has no missing keys" do
       I18n.locale = :fr
