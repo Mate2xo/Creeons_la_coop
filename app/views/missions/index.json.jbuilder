7056f624eb160b688a6ca525652ed87559d8a7ba
Tim <timothee.bull@gmail.com>
2019-12-12 18:46:05 +0100

    Apply conditional purle color on calendar events
    Condition: a mission.min_member_count is not reached yet, or no one knows how to
    handle the cash register.

--- app/views/missions/index.json.jbuilder
+++ app/views/missions/index.json.jbuilder
@@ -8,12 +8,13 @@ json.array! @missions do |mission|
   json.end mission.due_date
   json.url mission_path(mission.id)
 
-  # colors
-  if mission.members.length == mission.max_member_count
+  # events colors
+  members = mission.members
+  if members.length == mission.max_member_count
     json.color('grey')
-  elsif mission.members.length < mission.min_member_count
-    json.color('red')
-  elsif mission.members.length >= mission.min_member_count
+  elsif members.length < mission.min_member_count || members.all?(&:untrained?)
+    json.color('purple')
+  elsif members.length >= mission.min_member_count
     json.color('green')
   end
 end
--- spec/features/members/cash_register_proficiency_spec.rb
+++ spec/features/members/cash_register_proficiency_spec.rb
@@ -4,14 +4,13 @@ require 'rails_helper'
 
 RSpec.describe "Members cash register proficiency", type: :feature do
   let(:mission) { create :mission }
+  let(:jack) { create :member, cash_register_proficiency: :proficient }
+
+  before { sign_in jack }
 
   context "when on a mission details page" do
-    let(:sandy) { create :member }
-    let(:jack) { create :member, cash_register_proficiency: :proficient }
-
     before {
       mission.members << jack
-      sign_in sandy
       visit mission_path(mission.id)
     }
 
@@ -20,6 +19,18 @@ RSpec.describe "Members cash register proficiency", type: :feature do
         I18n.translate(jack.cash_register_proficiency,
                        scope: "activerecord.attributes.member.cash_register_proficiencies")
       )
+    end
+  end
+
+  context "when on the mission index page" do
+    before {
+      mission.members << create_list(:member, 3, cash_register_proficiency: :untrained)
+      visit missions_path
+    }
+
+    it "shows missions without proficient members in purple", js: true do
+      expect(find("a[href='/missions/#{mission.id}']").native.style('background-color'))
+        .to eq 'rgb(128, 0, 128)'
     end
   end
 end
