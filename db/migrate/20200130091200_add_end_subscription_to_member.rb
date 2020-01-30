808bcfdc1f4437d696d7daeb039aa6b653eec086
Floury-Fabio <floury-fabio@hotmail.fr>
2020-01-30 10:47:04 +0100

    correction of a mistake on file name

--- db/migrate/20200130091200_add_end_subscription_to_member.rb
+++ db/migrate/20200130091200_add_end_subscription_to_member.rb
@@ -1,4 +1,4 @@
-class AddEndSubrciptionToMember < ActiveRecord::Migration[5.2]
+class AddEndSubscriptionToMember < ActiveRecord::Migration[5.2]
   def change
 		add_column :member, :end_subscription, :datetime
   end
