7a4b1c2703f01c5770ef3c0ebe900e6c642f7afe
Floury-Fabio <floury-fabio@hotmail.fr>
2020-01-31 21:42:37 +0100

    add send notification job

---
+++ app/jobs/send_notification_job.rb
@@ -0,0 +1,11 @@
+class SendNotificationJob < ApplicationJob
+  queue_as :default
+
+  def perform(*_args)
+    end_subscription_period = {start_date: Date.today, end_date: Date.today + 15}
+    members = Member.where(":start_date <= end_subscription AND end_subscription <= :end_date", end_subscription_period).find_each(batch_size: 200) do |member|
+        MemberMailer.end_subscription_alert(member).deliver_now
+    end
+  end
+
+end
