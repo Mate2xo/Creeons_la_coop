d23f9845736f1998351608d6c264fd015a3deebf
Floury-Fabio <floury-fabio@hotmail.fr>
2020-01-31 20:22:11 +0100

    add test for display of end subscription in edit view

--- spec/views/members/edit.html.erb_spec.rb
+++ spec/views/members/edit.html.erb_spec.rb
@@ -5,8 +5,18 @@ require Rails.root.join('spec', 'support', 'shared_examples.rb')
 
 # rubocop: disable RSpec/DescribeClass
 RSpec.describe "members/edit" do
+
   it_behaves_like "a view without missing translations" do
     before { assign(:member, build_stubbed(:member)) }
+  end
+
+  context 'it display the end_subscription' do
+    it "displays end_subscription" do
+      assign(:member, build(:member, end_subscription: Date.new(2020, 10, 10) ))
+      allow(view).to receive(:current_member) { build_stubbed :member }
+      render
+      expect(rendered).to match match '10-10-2020'
+    end
   end
 end
 # rubocop: enable RSpec/DescribeClass
