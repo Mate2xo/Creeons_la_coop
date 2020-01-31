a8ddf45f474a62e92b512325a97ca48c87ae0566
Floury-Fabio <floury-fabio@hotmail.fr>
2020-01-31 21:42:37 +0100

    add send notification job

---
+++ app/mailers/member_mailer.rb
@@ -0,0 +1,11 @@
+class MemberMailer < ApplicationMailer
+
+	default from: 'no-reply@monsite.fr'
+
+  def alert_end_of_souscription_email(member)
+    @member = member
+
+    mail(to: @member.email, subject: "Notification de fin d'inscription")
+  end
+
+end
