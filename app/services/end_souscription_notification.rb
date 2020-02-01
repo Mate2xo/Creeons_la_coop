e39b6e009232d671e48984049d0f46261de4337e
Floury-Fabio <floury-fabio@hotmail.fr>
2020-02-01 17:21:09 +0100

    services notification added

--- app/services/end_souscription_notification.rb
+++ app/services/end_souscription_notification.rb
@@ -1,5 +1,13 @@
 class EndSouscriptionNotification 
 	def perform
-		SendNotificationJob.perform_now 
+
+		n = Time.now
+		start = Time.new(n.year, n.month, n.day, 23, 30)
+
+		schedule = IceCube::Schedule.new(start)
+		schedule.add_recurrence_rule IceCube::Rule.daily
+		singleton.each do
+			#SendNotificationJob.perform_now 
+		end
 	end
 end
