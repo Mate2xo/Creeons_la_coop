29a2cbfc58e9bf8342b724d41d435e3a309ed7c1
Floury-Fabio <floury-fabio@hotmail.fr>
2020-02-01 10:53:55 +0100

    modification expression in edit and show view'

--- app/jobs/send_notification_job.rb
+++ app/jobs/send_notification_job.rb
@@ -2,8 +2,14 @@ class SendNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+
 		members = Member.all
 
-    # Do something later
+		members.each do |member|
+			if member.end_subscription.present? && (member.end_subscription - Date.today <= 7)
+				puts member.end_subscription
+			end
+		end
+
   end
 end
