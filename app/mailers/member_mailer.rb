40eff646e22b639804c6ddaa961cc175141f11ce
Floury-Fabio <floury-fabio@hotmail.fr>
2020-02-06 20:27:39 +0100

    apply i18n on member manager feature

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class MemberMailer < ApplicationMailer
-  default from: 'no-reply@monsite.fr'
 
-  def alert_end_of_souscription_email(member)
+  default from: Rails.application.credentials.dig(:noreply_mail_address) || 'no-reply@example.com'
+  
+  def end_subscription_alert(member)
     @member = member
-
     mail(to: @member.email, subject: "Notification de fin d'inscription")
   end
 end
