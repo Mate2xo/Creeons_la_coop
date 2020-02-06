60111a49d6d7fdc470e19ea28e52a2a5f52a3eed
Floury-Fabio <floury-fabio@hotmail.fr>
2020-02-06 19:41:39 +0100

    update member mailer

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class MemberMailer < ApplicationMailer
-  default from: 'no-reply@monsite.fr'
 
+  default from: Rails.application.credentials.dig(:noreply_mail_address) || 'no-reply@example.com'
+  
   def alert_end_of_souscription_email(member)
-    @member = member
-
+    member = member
     mail(to: @member.email, subject: "Notification de fin d'inscription")
   end
 end
