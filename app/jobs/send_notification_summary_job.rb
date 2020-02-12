44b552ce2254686256480dddca2fbb8797c52136
Floury-Fabio <floury-fabio@hotmail.fr>
2020-02-12 11:11:21 +0100

    implementation send notification summary jog

--- app/jobs/send_notification_summary_job.rb
+++ app/jobs/send_notification_summary_job.rb
@@ -2,6 +2,10 @@ class SendNotificationSummaryJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+
+    admins = Member.where(role: 'admin')
+    admins.each do |admin|
+      AdminMailer.end_subscription_info(admin).deliver_now
+    end
   end
 end
