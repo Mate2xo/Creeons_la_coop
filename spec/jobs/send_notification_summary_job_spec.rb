9ecd43a63de003cec5db3745689273f117168e45
Floury-Fabio <floury-fabio@hotmail.fr>
2020-02-12 12:23:47 +0100

    add test for send notification job

--- spec/jobs/send_notification_summary_job_spec.rb
+++ spec/jobs/send_notification_summary_job_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe SendNotificationSummaryJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'when the mails is sended' do
+
+    it 'send mails only to the admins' do
+      ActiveJob::Base.queue_adapter = :test
+      SendNotificationSummaryJob.perform_now
+      admins = Member.where(role: 'admin')
+
+      admins.each do |admin|
+        expect(ActionMailer::Base.deliveries.any? {|m| m.to[0] == admin.email}).to eq true
+      end
+      expect(ActionMailer::Base.deliveries.size).to eq admins.size
+    end
+  end
 end
