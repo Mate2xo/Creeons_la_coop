13682e3f02d4b31758f5e4ed5750c834894f02a9
Floury-Fabio <floury-fabio@hotmail.fr>
2020-02-13 18:32:29 +0100

    add test in send_notification_job_spec

--- spec/jobs/send_notification_job_spec.rb
+++ spec/jobs/send_notification_job_spec.rb
@@ -14,35 +14,71 @@ RSpec.describe SendNotificationJob, :type => :job do
 
     context "when the job must send mails to the members" do
 
-     it "send a the correct number of mail" do
-        rand_number1 = rand(100)
-        rand_number2 = rand(100)
-        rand_number3 = rand(100)
+      it "send a mail to all members in end subscription period" do
 
-        create_list :member, rand_number1, end_subscription: Faker::Date.between(from: Date.today, to: Date.today + 15)
-        create_list :member, rand_number2, end_subscription: Date.today - rand(16..30)
-        create_list :member, rand_number3, end_subscription: nil
+
+        end_subscription_period_params = {from: Date.today, to: Date.today + 15}
+        members_number = rand(1..500) 
+
+        create_list :member, members_number, end_subscription: Faker::Date.between(end_subscription_period_params)
 
         ActiveJob::Base.queue_adapter = :test
         expect {
           SendNotificationJob.perform_now
-        }.to change { ActionMailer::Base.deliveries.size }.by(rand_number1) 
+        }.to change { ActionMailer::Base.deliveries.size }.by(members_number) 
+      end
+
+      it "not send a mail to members which are not in end subscription period" do
+
+        before_end_subscription_period_params = {from: Date.today + 16, to: Date.today + 300}
+        members_number = rand(1..500) 
+
+        create_list :member, members_number, end_subscription: Faker::Date.between(before_end_subscription_period_params)
+
+        ActiveJob::Base.queue_adapter = :test
+        expect {
+          SendNotificationJob.perform_now
+        }.to change { ActionMailer::Base.deliveries.size }.by(0) 
+      end
+     
+      it "not send a mail to members which have an outdated subscription date " do
+
+        before_end_subscription_period_params = {from: Date.today - 1, to: Date.today - 300}
+        members_number = rand(1..500) 
+
+        create_list :member, members_number, end_subscription: Faker::Date.between(before_end_subscription_period_params)
+
+        ActiveJob::Base.queue_adapter = :test
+        expect {
+          SendNotificationJob.perform_now
+        }.to change { ActionMailer::Base.deliveries.size }.by(0) 
+      end
+
+      it "not send a mail to members which have a nil subscription date " do
+
+        members_number = rand(1..500) 
+
+        create_list :member, members_number, nil
+
+        ActiveJob::Base.queue_adapter = :test
+        expect {
+          SendNotificationJob.perform_now
+        }.to change { ActionMailer::Base.deliveries.size }.by(0) 
       end
 
       it "send mails to the right members" do
-        member1 = create :member, end_subscription: Date.today - 10
-        member2 = create :member, end_subscription: Date.today - 30 
-        member3 = create :member, end_subscription: Date.today - 5
-        member4 = create :member, end_subscription: Date.today + 5
+        member_which_are_in_end_subscription_period = create :member, end_subscription: Date.today + 10
+        member_which_are_not_in_end_subscription_period = create :member, end_subscription: Date.today + 30 
+        member_which_have_a_outdated_end_subscription_date = create :member, end_subscription: Date.today - 5
+        member_which_have_a_nil_end_end_subscription_date = create :member, end_subscription: nil
         ActiveJob::Base.queue_adapter = :test
         SendNotificationJob.perform_now
         expect(ActionMailer::Base.deliveries.any? {|m| m.to[0] == member1.email}).to eq true
         expect(ActionMailer::Base.deliveries.any? {|m| m.to[0] == member2.email}).to eq false
-        expect(ActionMailer::Base.deliveries.any? {|m| m.to[0] == member3.email}).to eq true
+        expect(ActionMailer::Base.deliveries.any? {|m| m.to[0] == member3.email}).to eq false
         expect(ActionMailer::Base.deliveries.any? {|m| m.to[0] == member4.email}).to eq false
       end
+
     end
-
-
   end
 end
