c2d56ca2fc7918a100bbab8b0885817b3fe4f1a5
Floury-Fabio <floury-fabio@hotmail.fr>
2020-02-13 18:35:29 +0100

    add .find_each to the where querry

--- app/jobs/send_notification_job.rb
+++ app/jobs/send_notification_job.rb
@@ -5,8 +5,7 @@ class SendNotificationJob < ApplicationJob
 
 
   def perform(*_args)
-    members = Member.where(":start_date <= end_subscription AND end_subscription <= :end_date", {start_date: Date.today, end_date: Date.today + 15})
-    members.each do |member|
+    members = Member.where(":start_date <= end_subscription AND end_subscription <= :end_date", {start_date: Date.today, end_date: Date.today + 15}).find_each(batch_size: 200) do |member|
         MemberMailer.end_subscription_alert(member).deliver_now
     end
     
