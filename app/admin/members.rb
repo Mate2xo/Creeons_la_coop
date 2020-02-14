a2574bce6062f673a61da1c09d713716d37294d4
Floury-Fabio <floury-fabio@hotmail.fr>
2020-02-15 00:47:37 +0100

    add renew group action in active admin
    add i18n keys for renew group
    
    add alert for renew batch action

--- app/admin/members.rb
+++ app/admin/members.rb
@@ -5,6 +5,7 @@ ActiveAdmin.register Member do
   permit_params :email, :encrypted_password, :first_name, :last_name, :biography,
                 :phone_number, :role, :moderator, :group, :confirmed_at,
                 :password, :password_confirmation, :cash_register_proficiency
+                :end_subscription
 
   index do
     selectable_column
@@ -14,12 +15,14 @@ ActiveAdmin.register Member do
     column :group do |member| Member.human_enum_name(:group, member.group) end
     column :cash_register_proficiency
     column :email
+    column :end_subscription
     actions
   end
 
   form do |f|
     f.inputs :first_name, :last_name, :email, :phone_number, :role,
-             :moderator, :group, :cash_register_proficiency, :biography
+             :moderator, :group, :cash_register_proficiency,:end_subscription,
+             :biography
     actions
   end
 
@@ -30,9 +33,18 @@ ActiveAdmin.register Member do
   filter :role
   filter :group
   filter :cash_register_proficiency
+  filter :end_subscription
 
   action_item :invite_member, only: :index do
     link_to t("active_admin.invite_member"), new_member_invitation_path
   end
+
+  batch_action :renew, confirm: I18n.t("active_admin.batch_actions.renew_confirmation") do |ids|
+    batch_action_collection.find(ids).each do |member|
+      member.renew_subscription_date 
+    end
+    redirect_to collection_path, alert: t("active_admin.renew_member_alert")
+  end
+
 end
 # rubocop: enable Metrics/BlockLength
