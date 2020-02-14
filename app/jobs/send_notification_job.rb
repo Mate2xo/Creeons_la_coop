f4f8ba835d1679475f59fcf1fd298fc1f154dfa8
Floury-Fabio <floury-fabio@hotmail.fr>
2020-02-14 19:16:15 +0100

    rename date hash

--- app/jobs/send_notification_job.rb
+++ app/jobs/send_notification_job.rb
@@ -5,8 +5,8 @@ class SendNotificationJob < ApplicationJob
 
 
   def perform(*_args)
-    end_subscription_period_params = {start_date: Date.today, end_date: Date.today + 15}
-    members = Member.where(":start_date <= end_subscription AND end_subscription <= :end_date", end_subscription_period_params).find_each(batch_size: 200) do |member|
+    end_subscription_period = {start_date: Date.today, end_date: Date.today + 15}
+    members = Member.where(":start_date <= end_subscription AND end_subscription <= :end_date", end_subscription_period).find_each(batch_size: 200) do |member|
         MemberMailer.end_subscription_alert(member).deliver_now
     end
   end
