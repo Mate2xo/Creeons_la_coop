b890067ac5f5385d0f4a9e664884501a62959b5c
Tim <timothee.bull@gmail.com>
2020-04-19 12:54:16 +0200

    Partialize enrollments on missions/show

--- app/helpers/missions_helper.rb
+++ app/helpers/missions_helper.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
+# Helpers for views/missions
 module MissionsHelper
+  def partial_enrollment?(enrollment)
+    mission_start =  enrollment.mission.start_date.strftime('%T')
+    mission_end = enrollment.mission.due_date.strftime('%T')
+    member_begins_after_mission_start = enrollment.start_time.strftime('%T') != mission_start
+    member_finishes_before_mission_end = enrollment.end_time.strftime('%T') != mission_end
+
+    member_begins_after_mission_start || member_finishes_before_mission_end
+  end
+
+  def partial_enrollment_css
+    'bg-info rounded text-white p-1'
+  end
+
+  def enrollment_duration(enrollment)
+    "#{enrollment.start_time&.strftime('%H:%M')} - #{enrollment.end_time&.strftime('%H:%M')}"
+  end
 end
