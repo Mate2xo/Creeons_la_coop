590dfc774ffd35d7a75643adad261b4e6db62952
Tim <timothee.bull@gmail.com>
2020-05-10 16:19:26 +0200

    Show members worked hours on admin interface

--- app/admin/members.rb
+++ app/admin/members.rb
@@ -10,6 +10,7 @@ ActiveAdmin.register Member do
     column :first_name
     column :last_name
     column :role
+    column '3 heures faites?' do |member| member.worked_three_hours?(Date.current) end
     column :group do |member| Member.human_enum_name(:group, member.group) end
     column :cash_register_proficiency
     column :email
@@ -31,22 +32,25 @@ ActiveAdmin.register Member do
   filter :cash_register_proficiency
 
   action_item :invite_member, only: :index do
-    link_to t("active_admin.invite_member"), new_member_invitation_path
+    link_to t('active_admin.invite_member'), new_member_invitation_path
   end
 
   controller do
     def create(options = {}, &block)
       new_unloggable_member = build_resource
-      first_name, last_name = new_unloggable_member.first_name, new_unloggable_member.last_name
+      first_name = new_unloggable_member.first_name
+      last_name = new_unloggable_member.last_name
       new_unloggable_member.email = "#{first_name}.#{last_name}.#{Date.current}@compte.web.inactif"
 
-      def new_unloggable_member.password_required?; false; end
-
-      def new_unloggable_member.email_required?; false; end
-
-      if create_resource(new_unloggable_member)
-        options[:location] ||= smart_resource_url
+      def new_unloggable_member.password_required?
+        false
       end
+
+      def new_unloggable_member.email_required?
+        false
+      end
+
+      options[:location] ||= smart_resource_url if create_resource(new_unloggable_member)
 
       respond_with_dual_blocks(new_unloggable_member, options, &block)
     end
