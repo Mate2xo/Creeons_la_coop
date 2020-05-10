59e1206829503e4dbf393814741e2b451dfc7a2e
Tim <timothee.bull@gmail.com>
2020-05-10 13:20:26 +0200

    Set basic worked hours tracker in member profile

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
+# Helpers for Member-related views
 module MembersHelper
+  def month_total_hours(member, month_number)
+    member
+      .enrollments
+      .select { |enroll| enroll.mission.start_date.month == month_number }
+      .reduce(0.0) { |sum, enrollment| sum + enrollment.duration }
+  end
 end
