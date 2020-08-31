f5b616e86df6a8e57c4309f18a2cb75f95e46aa0
floury-fabio <floury-fabio@hotmail.fr>
2020-08-31 19:34:45 +0200

    rework seed with group Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@ require 'faker'
 Info.destroy_all
 Address.destroy_all
 Mission.destroy_all
+GroupMember.destroy_all
+Group.destroy_all
 Member.destroy_all
 Productor.destroy_all
 Document.destroy_all
@@ -12,48 +14,76 @@ Thredded::Messageboard.destroy_all
 Thredded::MessageboardGroup.destroy_all
 
 9.times do |index|
-  FactoryBot.create :member, :admin, :group,
+  FactoryBot.create :member, :admin,
                     address: FactoryBot.create(:address),
                     email: "admin#{index}@admin.com"
 end
-FactoryBot.create :member, :super_admin, :group,
-                  address: FactoryBot.create(:address),
-                  email: "super@admin.com"
-FactoryBot.create_list :member, 30, :group
-puts "Members seeded"
 
-10.times {
+FactoryBot.create :member, :super_admin,
+                  address: FactoryBot.create(:address),
+                  email: 'super@admin.com'
+FactoryBot.create_list :member, 30
+
+Rails.logger.info 'Members seeded'
+
+FactoryBot.create :group, :with_group_manager_mail, name: 'welcome'
+FactoryBot.create :group, name: 'financial_management'
+FactoryBot.create :group, name: 'members_management'
+FactoryBot.create :group, name: 'core'
+FactoryBot.create :group, name: 'schedule'
+FactoryBot.create :group, name: 'diy'
+FactoryBot.create :group, name: 'internal_culture'
+FactoryBot.create :group, name: 'local_suppliers'
+FactoryBot.create :group, name: 'other_suppliers'
+FactoryBot.create :group, name: 'supply'
+FactoryBot.create :group, name: 'orders_management'
+FactoryBot.create :group, name: 'it'
+
+Member.all.each do |member|
+  rand(4).times do
+    FactoryBot.create :group_member, member: member, group: Group.all.sample
+  end
+end
+
+Rails.logger.info 'groups seeded'
+
+10.times do
   FactoryBot.create :productor, address: FactoryBot.create(:address, :coordinates)
   FactoryBot.create :productor, local: true,
-                                address: FactoryBot.create(:address, :coordinates)
-}
-puts "Productors seeded"
+    address: FactoryBot.create(:address, :coordinates)
+end
 
-10.times {
+Rails.logger.info 'Productors seeded'
+
+10.times do
   FactoryBot.create :mission, members: Member.all.sample(rand(0..8)),
-                              addresses: FactoryBot.create_list(:address, rand(1..2)),
-                              author: Member.all.sample
-}
-puts "Missions seeded"
+  addresses: FactoryBot.create_list(:address, rand(1..2)),
+  author: Member.all.sample
+end
+
+Rails.logger.info 'Missions seeded'
 
 FactoryBot.create_list :info, 5, author: Member.all.sample
-puts "Infos seeded"
+Rails.logger.info 'Infos seeded'
 
 5.times do
   document = Document.new
-  document.file.attach(io: File.open('spec/support/fixtures/erd.pdf'), filename: 'erd.pdf', content_type: 'application/pdf')
+  document.file.attach(io: File.open('spec/support/fixtures/erd.pdf'),
+                       filename: 'erd.pdf', content_type: 'application/pdf')
   document.save!
 end
-puts "Documents seeded"
+
+Rails.logger.info 'Documents seeded'
 
 FactoryBot.create_list :messageboard_group, 2
 FactoryBot.create_list :messageboard, 6,
-                       messageboard_group_id: Thredded::MessageboardGroup.all.sample.id
+  messageboard_group_id: Thredded::MessageboardGroup.all.sample.id
 
-18.times {
+18.times do
   FactoryBot.create :topic, with_posts: rand(1..4),
-                            messageboard: Thredded::Messageboard.all.sample
-}
-puts "Forum seeded"
+  messageboard: Thredded::Messageboard.all.sample
+end
 
-puts "Database seed OK. App is ready to use"
+Rails.logger.info 'Forum seeded'
+
+Rails.logger.info 'Database seed OK. App is ready to use'
--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :group do
-    group_manager_mail { "MyString" }
+    name { Faker::Lorem.word }
+
+    trait :with_group_manager_mail do
+      group_manager_mail { Member.all.sample.email }
+    end
   end
 end
