ca1dfb115ea1cc6137302d50450e6e3a1760aa32
floury-fabio <floury-fabio@hotmail.fr>
2020-09-02 10:27:41 +0200

    adapt rspec test to new group management

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,31 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: missions
+#
+#  id                       :bigint(8)        not null, primary key
+#  name                     :string           not null
+#  group_manager_mail       :string
+
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Model instanciation' do
+    subject { described_class.new }
+
+    describe 'Database' do
+      it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
+      it { is_expected.to have_db_column(:group_manager_mail).of_type(:string) }
+    end
+
+    describe 'validations' do
+      it { is_expected.to validate_presence_of(:name) }
+      it { validate_uniqueness_of(:name).case_insensitive }
+    end
+
+    describe 'associations' do
+      it { is_expected.to have_many(:members).through(:group_members) }
+    end
+  end
 end
--- spec/views/members/index.html.erb_spec.rb
+++ spec/views/members/index.html.erb_spec.rb
@@ -6,7 +6,10 @@ require Rails.root.join('spec', 'support', 'shared_examples.rb')
 # rubocop: disable RSpec/DescribeClass
 RSpec.describe "members/index" do
   it_behaves_like "a view without missing translations" do
-    before { assign(:members, build_stubbed_list(:member, 5)) }
+    before {
+      assign(:members, build_stubbed_list(:member, 5))
+      assign(:groups, build_stubbed_list(:group, 5))
+    }
   end
 end
 # rubocop: enable RSpec/DescribeClass
