e71a18dfc63eefdceff2cd327be35d9aeaf89e90
floury-fabio <floury-fabio@hotmail.fr>
2020-09-02 12:15:02 +0200

    add groups list in show view

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -2,4 +2,11 @@
 
 # Helpers for Member-related views
 module MembersHelper
+  def change_groups_collection_in_to_string(groups)
+    names_groups = ''
+    groups.each do |group|
+      names_groups = names_groups + Member.human_enum_name(:group, group.name) + ', '
+    end
+    names_groups.delete_suffix(', ')
+  end
 end
