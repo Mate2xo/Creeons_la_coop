b8df7d29acc9d7f922c384e2f7f0c96bdfae2bbb
floury-fabio <floury-fabio@hotmail.fr>
2020-10-09 12:53:58 +0200

    add group management in active admin

---
+++ app/admin/groups.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# rubocop: disable Metrics/BlockLength
+#
+#
+
+ActiveAdmin.register Group do
+  permit_params :name, :group_manager_mail
+
+  index do
+    selectable_column
+    column :name
+    column :group_manager_mail
+    column(I18n.t("activerecord.attributes.group.number_of_members")) { |group| group.members.size }
+    actions
+  end
+
+  show do
+    attributes_table_for resource do
+      default_attribute_table_rows.each do |field|
+        row field
+      end
+      table_for group.members do
+        column 'members' do |member|
+          link_to "#{member.last_name} #{member.first_name}", [:admin, member]
+        end
+      end
+    end
+  end
+end
+# rubocop: enable Metrics/BlockLength
