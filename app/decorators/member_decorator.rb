7f4f037f0351a4d9ebcac620e12b13bac9c319bc
floury-fabio <floury-fabio@hotmail.fr>
2020-11-04 18:28:08 +0100

    add month localization

--- app/decorators/member_decorator.rb
+++ app/decorators/member_decorator.rb
@@ -4,17 +4,17 @@ class MemberDecorator < Draper::Decorator
   def worked_hours_of_three_last_months
     hours_per_month = []
     3.times do |n|
-      hours_per_month[n] = h.content_tag(:p, "#{(Date.current - n.month).strftime('%B')} :
+      hours_per_month[n] = h.content_tag(:p, "#{I18n.localize(Date.current - n.month, format: :only_month)} :
                                               #{model.monthly_worked_hours(Date.current - n.month)}")
     end
     h.safe_join hours_per_month.reverse
   end
 
-
   def worked_hours_of_three_last_months_csv_version
     hours_per_month = []
     3.times do |n|
-      hours_per_month[n] = "#{(Date.current - n.month).strftime('%B')} : #{model.monthly_worked_hours(Date.current - n.month)}"
+      hours_per_month[n] = "#{I18n.localize(Date.current - n.month, format: :only_month)} :
+                            #{model.monthly_worked_hours(Date.current - n.month)}"
     end
     hours_per_month.reverse.join("\n")
   end
