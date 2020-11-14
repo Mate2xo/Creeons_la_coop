dc8b2cb2f6b301b08a131a41e92a2d4b4b0558b4
floury-fabio <floury-fabio@hotmail.fr>
2020-11-14 11:57:42 +0100

    refactor #hours_worked_in_the_last_three_months

--- app/decorators/member_decorator.rb
+++ app/decorators/member_decorator.rb
@@ -4,21 +4,15 @@
 class MemberDecorator < Draper::Decorator
   delegate_all
 
-  def hours_worked_in_the_three_last_months(csv: false)
-    hours_per_month = []
-    if csv == false
-      3.times do |n|
-        hours_per_month[n] = h.content_tag(:p, "#{I18n.localize(Date.current - n.month, format: :only_month)} :
-                                              #{model.monthly_worked_hours(Date.current - n.month)}")
-      end
-      h.safe_join hours_per_month.reverse
-    else
-      3.times do |n|
-        hours_per_month[n] = "#{I18n.localize(Date.current - n.month, format: :only_month)} :
-                            #{model.monthly_worked_hours(Date.current - n.month)}"
-      end
+  def hours_worked_in_the_last_three_months(csv: false)
+    hours_per_month = 3.times.with_object([]) do |n, array|
+      array[n] = "#{I18n.localize(Date.current - n.month, format: :only_month)} :
+                                  #{model.monthly_worked_hours(Date.current - n.month)}"
+    end
+    if csv
       hours_per_month.reverse.join("\n")
-
+    else
+      h.safe_join(hours_per_month.map { |month_total| h.content_tag(:p, month_total) }.reverse)
     end
   end
 end
