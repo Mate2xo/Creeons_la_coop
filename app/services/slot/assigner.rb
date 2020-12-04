f904d7d244d4704255bfdac10fbc7cf7768dd494
floury-fabio <floury-fabio@hotmail.fr>
2020-12-04 15:04:24 +0100

    add transaction for slots assignement and add test on it

--- app/services/slot/assigner.rb
+++ app/services/slot/assigner.rb
@@ -12,15 +12,21 @@ class Slot::Assigner < ApplicationService
   end
 
   def assign
-    update_slots_in_order_to_remove_these_from_member
-    update_slots_in_order_to_give_these_to_member
-    return false if @errors.any?
+    all_is_ok = true
+    Mission::Slot.transaction do
+      update_slots_in_order_to_remove_these_from_member
+      update_slots_in_order_to_give_these_to_member
+      raise ActiveRecord::Rollback if @errors.any?
+    rescue ActiveRecord::Rollback
+      all_is_ok = false
+    end
 
-    true
+    all_is_ok
   end
 
   def update_slots_in_order_to_remove_these_from_member
-    deselected_slots = Mission::Slot.where(member_id: @member_id, mission_id: @mission_id).where.not(start_time: @start_times)
+    deselected_slots = Mission::Slot.where(member_id: @member_id, mission_id: @mission_id)
+                                    .where.not(start_time: @start_times)
     deselected_slots.each do |slot|
       slot.update(member_id: nil) || @errors << slot.update_errors.full_messages.join(', ')
     end
@@ -42,11 +48,12 @@ class Slot::Assigner < ApplicationService
 
   def give_a_slot_with_a_start_time(start_time)
     slot = Mission::Slot.find_by(mission_id: @mission_id, member_id: nil, start_time: start_time)
-    if slot.present?
-      slot.update(member_id: @member_id)
-      @errors << slot.errors.full_messages.join(', ') if slot.errors.present?
-    else
-      @errors << I18n.t('activerecord.errors.slot.unavailability', start_time: start_time)
+    if slot.nil?
+      @errors << I18n.t('activerecord.errors.models.slot.messages.unavailability', start_time: start_time)
+      return
     end
+
+    slot.update(member_id: @member_id)
+    @errors << slot.errors.full_messages.join(', ') if slot.errors.present?
   end
 end
--- spec/requests/slots_spec.rb
+++ spec/requests/slots_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'Slots request', type: :request do
 
     it "update a slot with the member's id" do
       put mission_slot_path(mission.id, mission.slots.first.id),
-        params: { slot: { member_id: member.id, start_times: [mission.start_date] } }
+          params: { slot: { member_id: member.id, start_times: [mission.start_date] } }
 
       expect(mission.slots.where(member_id: member.id, start_time: mission.start_date).count).to eq 1
     end
@@ -29,25 +29,38 @@ RSpec.describe 'Slots request', type: :request do
         slot.update(member_id: member.id)
 
         put mission_slot_path(mission.id, mission.slots.first.id),
-          params: { slot: { member_id: member.id } }
+            params: { slot: { member_id: member.id } }
 
         expect(slot.reload.member_id).to be_nil
       end
     end
 
-    context 'when the time slots are unavailable' do
-      it "doesn't update" do
-        generate_enrollments_on_n_time_slots_of_a_mission(4)
+    context 'when the one time slots are unavailable' do
+      let(:slot_params) do
+        { slot: { member_id: member.id,
+                  start_times: [mission.start_date, mission.start_date + 90.minutes] } }
+      end
 
-        put mission_slot_path(mission.id, mission.slots.first.id),
-          params: { slot: { member_id: member.id,
-                            start_times: [mission.start_date] } }
+      it "don't update any slots" do
+        generate_enrollments_on_n_time_slots_of_a_mission(mission, 4)
+        mission.slots.find_by(start_time: mission.start_date)
+
+        put mission_slot_path(mission.id, mission.slots.first.id), params: slot_params
+
         expect(Mission::Slot.where(mission_id: mission.id, member_id: member.id).count).to eq 0
+      end
+
+      it 'warns on time_slot unavailability' do
+        generate_enrollments_on_n_time_slots_of_a_mission(mission, 4)
+
+        put mission_slot_path(mission.id, mission.slots.first.id), params: slot_params
+
+        expect(response.body).to include(I18n.t('activerecord.errors.models.slot.messages.unavailability',
+                                                start_time: mission.start_date.strftime('%FT%T%:z')))
       end
     end
 
-
-    def generate_enrollments_on_n_time_slots_of_a_mission(members_count)
+    def generate_enrollments_on_n_time_slots_of_a_mission(mission, members_count)
       members = create_list :member, members_count
       members.each do |member|
         enroll(mission, member)
@@ -55,4 +68,3 @@ RSpec.describe 'Slots request', type: :request do
     end
   end
 end
-
