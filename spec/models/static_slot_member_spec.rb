9d00ee569b597eac8a5362e8747cef3afc73c975
floury-fabio <floury-fabio@hotmail.fr>
2020-12-12 11:10:25 +0100

    add tests for static slot

--- spec/models/static_slot_member_spec.rb
+++ spec/models/static_slot_member_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe StaticSlotMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:static_slot) }
+  it { is_expected.to belong_to(:member) }
 end
--- spec/models/static_slot_spec.rb
+++ spec/models/static_slot_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe StaticSlot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Model instanciation' do
+    describe 'validations' do
+      it { is_expected.to validate_presence_of(:week_day) }
+      it { is_expected.to validate_presence_of(:hours) }
+      it { is_expected.to validate_presence_of(:week_type) }
+    end
+
+    describe 'associations' do
+      it { is_expected.to have_many(:members).through(:static_slot_member) }
+    end
+  end
 end
