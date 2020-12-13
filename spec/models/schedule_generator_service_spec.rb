51021f25123e6bd2ee9d057ae0718147ac042aa3
floury-fabio <floury-fabio@hotmail.fr>
2020-12-14 00:05:36 +0100

    explicite schedule generator test

--- spec/models/schedule_generator_service_spec.rb
+++ spec/models/schedule_generator_service_spec.rb
@@ -4,13 +4,13 @@ require 'rails_helper'
 
 RSpec.describe ScheduleGenerator, type: :model do
   describe '#generate_schedule' do
-    subject(:generate_schedule) { ScheduleGenerator.new(current_member).generate_schedule }
+    subject(:generate_schedule) { ScheduleGenerator.new(author).generate_schedule }
 
-    let(:current_member) { create :member }
+    let(:author) { create :member }
 
     before { allow(DateTime).to receive(:current).and_return DateTime.new(2020, 12, 10) }
 
-    it 'generates missions for the next month' do
+    it 'generates three missions for each working days of the next month ' do
       expect { generate_schedule }.to change(Mission, :count).by(78)
     end
   end
