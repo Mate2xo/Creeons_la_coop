279e1e012a4bd0275e3baebd4e568ce0c8babfbf
floury-fabio <floury-fabio@hotmail.fr>
2020-12-24 17:54:38 +0100

    add genre in strong parameters of mission

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -75,7 +75,7 @@ class MissionsController < ApplicationController
       :name, :description, :event, :delivery_expected,
       :recurrent, :recurrence_rule, :recurrence_end_date,
       :max_member_count, :min_member_count,
-      :due_date, :start_date,
+      :due_date, :start_date, :genre,
       addresses_attributes: %i[id postal_code city street_name_1 street_name_2 _destroy],
       enrollments_attributes: %i[id _destroy member_id start_time end_time]
     )
