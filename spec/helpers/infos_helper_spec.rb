575fb0dc1aeb59200766f685d9d2e904e63c783f
floury-fabio <floury-fabio@hotmail.fr>
2021-01-15 19:32:33 +0100

    add test for infos helpers #info_of_this_category

---
+++ spec/helpers/infos_helper_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe EnrollmentsHelper, type: :helper do
+  describe '#infos_of_this_category' do
+    it 'returns the infos of the indicated category' do
+      event_infos = create_list :info, 2, category: 'event'
+      create :info, category: 'management'
+
+      returned_infos = helper.infos_of_this_category('event')
+
+      expect([returned_infos]).to include(event_infos)
+    end
+
+    it "doesn't return the infos of an other category that the indicated category" do
+      create_list :info, 2, category: 'event'
+      managment_info = create :info, category: 'management'
+
+      returned_infos = helper.infos_of_this_category('event')
+
+      expect([returned_infos]).not_to include(managment_info)
+    end
+  end
+end
