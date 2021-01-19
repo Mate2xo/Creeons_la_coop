86462f8bf9ccd3fa6d48dcb62709c7332de27c70
floury-fabio <floury-fabio@hotmail.fr>
2021-01-19 13:28:13 +0100

    correct spelling errors

--- spec/models/static_members_recruiter_service_spec.rb
+++ spec/models/static_members_recruiter_service_spec.rb
@@ -3,30 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe StaticMembersRecruiter, type: :model do
-  subject(:static_members_recruite) { StaticMembersRecruiter.new.call }
+  subject(:recruit_static_members) { StaticMembersRecruiter.new.call }
 
   let(:member) { create :member }
 
   before { allow(DateTime).to receive(:current).and_return DateTime.new(2020, 12, 10) }
 
-  context 'when the member has one :static_slot corresponding to the mission' do
-    let(:create_static_slot_and_enroll_member_that_one) do
+  context 'when the member has one static_slot corresponding to a mission' do
+    let(:create_static_slot_and_enroll_a_member_on_it) do
       static_slot = create :static_slot, week_day: 'Monday', week_type: 'A', start_time: DateTime.new(2021, 1, 4, 9)
       create :member_static_slot, member: member, static_slot: static_slot
     end
 
-    it 'enroll members with :static_slot to this mission' do
-      create_static_slot_and_enroll_member_that_one
+    it 'enrolls members having this static_slot to this mission' do
+      create_static_slot_and_enroll_a_member_on_it
       mission = create :mission, start_date: DateTime.new(2021, 1, 4, 9), genre: 'regulated'
 
-      static_members_recruite
+      recruit_static_members
 
       expect(mission.members).to include(member)
     end
   end
 
-  context 'when the member has two :static_slots corresponding to the mission' do
-    let(:create_static_slots_and_enroll_member_those_there) do
+  context 'when the member has two static_slots corresponding to a mission' do
+    let(:create_two_static_slots_and_enroll_a_member_on_those) do
       static_slots = []
       static_slots << (create :static_slot, week_day: 'Monday', week_type: 'A', start_time: DateTime.new(2021, 1, 4, 9))
       static_slots << (create :static_slot,
@@ -39,11 +39,11 @@ RSpec.describe StaticMembersRecruiter, type: :model do
       end
     end
 
-    it 'enroll members with :static_slots to this mission' do
+    it 'enrolls members having this static_slot to this mission' do
       mission = create :mission, start_date: DateTime.new(2021, 1, 4, 9), genre: 'regulated'
-      create_static_slots_and_enroll_member_those_there
+      create_two_static_slots_and_enroll_a_member_on_those
 
-      static_members_recruite
+      recruit_static_members
 
       expect(mission.members).to include(member)
     end
