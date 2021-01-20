63fd6cb4ac295ed4c44f4c8c1fe1fafd402b12ae
floury-fabio <floury-fabio@hotmail.fr>
2021-01-20 19:26:27 +0100

    add published to info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_110415) do
+ActiveRecord::Schema.define(version: 2021_01_20_181957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,6 +142,7 @@ ActiveRecord::Schema.define(version: 2021_01_15_110415) do
     t.datetime "updated_at", null: false
     t.bigint "author_id"
     t.string "category"
+    t.boolean "published", default: false
     t.index ["author_id"], name: "index_infos_on_author_id"
   end
 
