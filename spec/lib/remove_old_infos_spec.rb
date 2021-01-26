54cef822656479eb88890d577d858d03b1025908
floury-fabio <floury-fabio@hotmail.fr>
2021-01-26 17:50:31 +0100

    correct spelling errors

--- spec/lib/remove_old_infos_spec.rb
+++ spec/lib/remove_old_infos_spec.rb
@@ -5,7 +5,7 @@ require 'rake'
 Rake.application.rake_require 'tasks/remove_old_infos'
 
 RSpec.describe 'info:remove_old_infos', type: :feature do
-  it 'removes the infos whose the last update dateback to more than one month' do
+  it 'deletes Info records that are more than one month old' do
     create :info, updated_at: DateTime.current - 2.months
 
     Rake::Task['info:remove_old_infos'].execute
@@ -13,7 +13,7 @@ RSpec.describe 'info:remove_old_infos', type: :feature do
     expect(Info.count).to eq 0
   end
 
-  it "doesn't remove infos whose the last update dateback to less than one month" do
+  it "doesn't delete Info records that are more than one month old" do
     create :info, updated_at: DateTime.current - 2.weeks
 
     Rake::Task['info:remove_old_infos'].execute
