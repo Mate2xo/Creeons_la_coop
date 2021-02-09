be71f2c9cf249b622f341df6952909a0053eba94
floury-fabio <floury-fabio@hotmail.fr>
2021-02-09 19:07:41 +0100

    config connection restrain at admin and super admins

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require cable
 //= require activestorage
 //= require turbolinks
 //= require jquery/jquery.min
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
 module ApplicationCable
-  class Connection < ActionCable::Connection::Base
+  class Connection < ActionCable::Connection::Base # rubocop:disable Style/Documentation
+    identified_by :current_member
+
+    def connect
+      self.current_member = find_verified_member
+    end
+
+    private
+
+    def find_verified_member
+      verified_user = env['warden'].user
+      return reject_unauthorized_connection unless verified_user
+
+      if verified_user.super_admin? || verified_user.admin?
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
