4da550b03e29b966bd1cb4fe513e1f9fcb5f8b3d
floury-fabio <floury-fabio@hotmail.fr>
2021-03-16 16:34:19 +0100

    add test for update admin mission this test check the control on the related enrollments before the mission update

--- spec/requests/admin/missions_spec.rb
+++ spec/requests/admin/missions_spec.rb
@@ -37,24 +37,224 @@ RSpec.describe 'A Missions admin request', type: :request do
         expect(Mission.last[:genre]).to eq 'event'
       end
     end
+
+    context 'when the duration is negative' do
+      let(:mission_params) do
+        attributes_for(:mission, start_date: DateTime.current, due_date: DateTime.current - 5.minutes)
+      end
+
+      it 'sends an error message when due date is inferior to start_date' do
+        post_mission
+
+        expect(response.body).to include(I18n.t('activerecord.errors.models.mission.attributes.duration.minimum'))
+      end
+    end
+
+    context 'when the duration is superior to ten hours' do
+      let(:mission_params) do
+        attributes_for(:mission, start_date: DateTime.current, due_date: DateTime.current + 11.hours)
+      end
+
+      it 'displays an error message' do
+        post_mission
+
+        expect(response.body).to include(I18n.t('activerecord.errors.models.mission.attributes.duration.maximum'))
+      end
+    end
+
+    context 'when the mission is regulated and the duration is not a multiple of 1.5 hours' do
+      let(:mission_params) do
+        attributes_for(:mission, genre: 'regulated', start_date: DateTime.current, due_date: DateTime.current + 1.hour)
+      end
+
+      it 'displays an error message' do
+        post_mission
+
+        expect(response.body).to include(I18n.t('activerecord.errors.models.mission.attributes.duration.multiple'))
+      end
+    end
   end
 
   describe 'PUT' do
-    subject(:put_mission) { put admin_mission_path(updated_mission.id), params: { mission: mission_params } }
+    subject(:put_mission) { put admin_mission_path(mission.id), params: { mission: mission_params } }
+
+    let(:mission) { create :mission, start_date: DateTime.current + 2.days }
+    let(:mission_params) do
+      attributes_for :mission,
+                     name: 'updated_mission',
+                     start_date: mission.start_date + 3.hours,
+                     due_date: mission.start_date + 6.hours
+    end
 
     before { allow(DateTime).to receive(:current).and_return DateTime.new(2020, 2, 3, 9) }
 
+    it 'updates the mission' do
+      put_mission
+
+      expect(mission.reload.attributes).to include(mission_params.stringify_keys)
+    end
+
+    it 'confirms the updates' do
+      put_mission
+      follow_redirect!
+
+      expect(response.body).to include(I18n.t('missions.update.confirm_update'))
+    end
+
+    context 'when the mission is :regulated and the params standard is passed' do
+      let(:mission) { create :mission, start_date: DateTime.current + 2.days, genre: 'regulated' }
+
+      let(:mission_params) do
+        attributes_for :mission,
+                       name: 'updated_mission',
+                       start_date: mission.start_date,
+                       due_date: mission.due_date,
+                       genre: 'standard'
+      end
+
+      it 'confirm the update' do
+        put_mission
+        follow_redirect!
+
+        expect(response.body).to include(I18n.t('missions.update.confirm_update'))
+      end
+    end
+
+    context 'when the duration is negative' do
+      let(:mission_params) do
+        attributes_for(:mission, start_date: DateTime.current, due_date: DateTime.current - 5.minutes)
+      end
+
+      it 'sends an error message when due date is inferior to start_date' do
+        put_mission
+
+        expect(response.body).to include(I18n.t('activerecord.errors.models.mission.attributes.duration.minimum'))
+      end
+    end
+
+    context 'when the duration is superior to ten hours' do
+      let(:mission_params) do
+        attributes_for(:mission, start_date: DateTime.current, due_date: DateTime.current + 11.hours)
+      end
+
+      it 'displays an error message' do
+        put_mission
+
+        expect(response.body).to include(I18n.t('activerecord.errors.models.mission.attributes.duration.maximum'))
+      end
+    end
+
+    context 'when the mission is regulated and the duration is not a multiple of 1.5 hours' do
+      let(:mission_params) do
+        attributes_for(:mission, genre: 'regulated', start_date: DateTime.current, due_date: DateTime.current + 1.hour)
+      end
+
+      it 'displays an error message' do
+        put_mission
+
+        expect(response.body).to include(I18n.t('activerecord.errors.models.mission.attributes.duration.multiple'))
+      end
+    end
+    context "when the mission have several enrollments and the datetimes of the related enrollments are outside
+    of the new mission's period" do
+      let(:create_enrollments) do
+        create_list :enrollment,
+                    3,
+                    start_time: mission.start_date,
+                    end_time: mission.due_date,
+                    member_id: (create :member).id,
+                    mission_id: mission.id
+      end
+
+      let(:mission_params) do
+        { name: 'updated_mission',
+          start_date: mission.start_date + 3.hours,
+          due_date: mission.due_date + 3.hours }
+      end
+
+      let(:expected_params) { { start_date: mission_params['start_date'], due_date: mission_params['due_date'] } }
+      let(:i18n_key) { 'activerecord.errors.models.mission.inconsistent_datetimes_for_related_enrollments' }
+
+      it 'displays an error message' do
+        create_enrollments
+
+        put_mission
+
+        expect(response.body).to include(I18n.t(i18n_key))
+      end
+    end
+
+    context "when the :regulate type is passed in params and the datetimes of the related enrollments
+    mismatch the mission's time_slots" do
+      let(:mission_params) do
+        attributes_for :mission,
+                       name: 'updated_mission',
+                       start_date: mission.start_date,
+                       due_date: mission.due_date,
+                       genre: 'regulated'
+      end
+
+      let(:create_enrollments) do
+        create_list :enrollment,
+                    3,
+                    start_time: mission.start_date + 1.hour,
+                    end_time: mission.start_date + 2.hours,
+                    member_id: (create :member).id,
+                    mission_id: mission.id
+      end
+
+      let(:i18n_key) { 'activerecord.errors.models.mission.mismatch_between_time_slots_and_related_enrollments' }
+
+      it 'displays a error message' do
+        create_enrollments
+
+        put_mission
+
+        expect(response.body).to include(I18n.t(i18n_key))
+      end
+    end
+
+    context "when the mission is :regulate, new datetimes are passed in params and the datetimes of the related
+    enrollments mismatch the time_slots of the new mission's period" do
+      let(:mission) { create :mission, start_date: DateTime.current + 2.days, genre: 'regulated' }
+
+      let(:mission_params) do
+        attributes_for :mission,
+                       name: 'updated_mission',
+                       start_date: mission.start_date - 1.hour,
+                       due_date: mission.due_date - 1.hour
+      end
+
+      let(:create_enrollments) do
+        create_list :enrollment,
+                    3,
+                    start_time: mission.start_date,
+                    end_time: mission.start_date + 90.minutes,
+                    member_id: (create :member).id,
+                    mission_id: mission.id
+      end
+
+      let(:i18n_key) { 'activerecord.errors.models.mission.mismatch_between_time_slots_and_related_enrollments' }
+
+      it 'displays a error message' do
+        create_enrollments
+
+        put_mission
+
+        expect(response.body).to include(I18n.t(i18n_key))
+      end
+    end
+
     context 'when the recurrent changes params is true' do
-      let(:updated_mission) { create :mission, start_date: DateTime.current + 2.days }
       let(:mission_params) do
         { name: 'updated_mission',
           recurrent_change: true,
-          start_date: updated_mission.start_date + 3.hours,
-          due_date: updated_mission.due_date + 3.hours }
+          start_date: mission.start_date + 3.hours,
+          due_date: mission.due_date + 3.hours }
       end
 
       it 'updates futures missions that match the same week day, hour, and genre' do # rubocop:disable Layout/LineLength
-        other_missions = create_future_matching_missions(updated_mission)
+        other_missions = create_future_matching_missions(mission)
 
         put_mission
 
@@ -64,7 +264,7 @@ RSpec.describe 'A Missions admin request', type: :request do
       end
 
       it "doesn't update pasts missions that match the same week day, hour, and genre" do # rubocop:disable Layout/LineLength
-        other_mission = create :mission, start_date: updated_mission.start_date - 2.days
+        other_mission = create :mission, start_date: mission.start_date - 2.days
 
         put_mission
 
@@ -72,11 +272,11 @@ RSpec.describe 'A Missions admin request', type: :request do
       end
 
       it "doesn't update :start_date attribute" do
-        expect { put_mission }.not_to change(updated_mission, :start_date)
+        expect { put_mission }.not_to change(mission, :start_date)
       end
 
       it "doesn't update :due_date attribute" do
-        expect { put_mission }.not_to change(updated_mission, :due_date)
+        expect { put_mission }.not_to change(mission, :due_date)
       end
     end
   end
