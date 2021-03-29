c364e1d1b53b598bc011c66bb2c0ffd00193bd0b
floury-fabio <floury-fabio@hotmail.fr>
2021-03-29 08:58:35 +0200

    migrate function attribute for group member

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_001313) do
+ActiveRecord::Schema.define(version: 2021_03_29_064814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 2021_02_01_001313) do
     t.bigint "member_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "function"
     t.index ["group_id"], name: "index_group_members_on_group_id"
     t.index ["member_id"], name: "index_group_members_on_member_id"
   end
