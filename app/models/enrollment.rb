ff29e5dcd3e4c591de07b576ae0a1f27e759c6c1
floury-fabio <floury-fabio@hotmail.fr>
2021-06-20 22:47:17 +0200

    refact enrollment model rm useless methods

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -27,24 +27,13 @@ class Enrollment < ApplicationRecord
   validates_with EnrollmentValidators::MatchingMissionTimeSlotsValidator
 
   def duration
-    return 0 if start_time == nil || end_time == nil
+    return 0 if start_time.nil? || end_time.nil?
 
     ((end_time - start_time) / 60 / 60).round 1
   end
 
   def contain_this_time_slot?(time_slot)
     start_time <= time_slot && time_slot < end_time
-  end
-
-
-  def check_if_the_duration_is_positive
-    if start_time >= end_time
-      failure_message = I18n.t('activerecord.errors.models.enrollment.negative_duration')
-      errors.add :negative_duration, failure_message
-      return false
-    end
-
-    true
   end
 
   private
