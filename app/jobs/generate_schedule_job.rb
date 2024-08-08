782b7c63bff470c692e68572936e7ce5168ef744
Tim <timothee.bull@gmail.com>
2024-08-08 14:34:20 +0200

    fix: History creation wouldn't have worked, as `args` var did not exist anymore

--- app/jobs/generate_schedule_job.rb
+++ app/jobs/generate_schedule_job.rb
@@ -3,11 +3,11 @@
 class GenerateScheduleJob < ApplicationJob # rubocop:disable  Style/Documentation
   queue_as :default
 
-  def perform(params)
-    schedule_generator = ScheduleGenerator.new(params[:current_member], params[:current_month].to_datetime)
+  def perform(options)
+    schedule_generator = ScheduleGenerator.new(options[:current_member], options[:current_month].to_datetime)
     schedule_generator.generate_schedule
     return unless schedule_generator.errors.empty?
 
-    HistoryOfGeneratedSchedule.create(month_number: args[0][:current_month].to_datetime)
+    HistoryOfGeneratedSchedule.create(month_number: options[:current_month].to_datetime)
   end
 end
