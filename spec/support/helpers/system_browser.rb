37983ccd9db066cb4f30cddb3831ea9971bd6de8
Tim <timothee.bull@gmail.com>
2025-05-21 11:09:11 +0200

    test: remove unused helper

--- spec/support/helpers/system_browser.rb
+++ spec/support/helpers/system_browser.rb
@@ -7,10 +7,6 @@ module SystemBrowser
     driven_by(:rack_test)
   end
 
-  def wait_for_flash_messages
-    expect(page).to have_css('div#flash_messages')
-  end
-
   def wait_flash(color)
     type = {success: :notice, warning: :alert, danger: :error}[color]
     expect(page).to have_css("div#flash_#{type}.alert-#{color}")
