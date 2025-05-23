072457c7460e425df04eef0b4cf6f41add66293b
Tim <timothee.bull@gmail.com>
2025-05-23 13:55:09 +0200

    config: variabilize default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :mailjet
-  config.action_mailer.default_url_options = {host: 'https://creons-la-coop.herokuapp.com/'}
+  config.action_mailer.default_url_options = {host: Rails.application.credentials.host}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
