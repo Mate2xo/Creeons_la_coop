c28d87d44e1ec873bfcf055c3197e1c921202da6
Tim <timothee.bull@gmail.com>
2025-05-30 11:00:40 +0200

    test: improve coverage

--- spec/requests/members_spec.rb
+++ spec/requests/members_spec.rb
@@ -2,16 +2,47 @@
 
 require 'rails_helper'
 
-RSpec.describe 'A Member request', type: :request do
-  let(:member) { create :member }
+RSpec.describe '/members' do
+  let(:member) { create(:member) }
 
   before { sign_in member }
 
-  describe 'Put update' do
-    subject(:put_member) { put member_path(member.id), params: { member: member_params } }
+  describe 'GET /' do
+    subject(:index) { create_list(:member, 2) and get members_path }
+
+    it 'has an HTTP :OK status' do
+      index
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET /:id' do
+    subject(:show) { get member_path(member) }
+
+    let(:member) { create(:member) }
+
+    it 'has an HTTP :OK status' do
+      show
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET /:id/edit' do
+    subject(:edit) { get edit_member_path(member) }
+
+    let(:member) { create(:member) }
+
+    it 'has an HTTP :OK status' do
+      edit
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'PUT /:id' do
+    subject(:put_member) { put member_path(member.id), params: {member: member_params} }
 
     context 'without static slots selection' do
-      let(:member_params) { { first_name: 'updated_member', last_name: 'tati' } }
+      let(:member_params) { {first_name: 'updated_member', last_name: 'tati'} }
 
       it 'update successfully' do
         put_member
@@ -23,13 +54,13 @@ RSpec.describe 'A Member request', type: :request do
     end
 
     context 'with static slots selection' do
-      let(:static_slot1) { create :static_slot }
-      let(:static_slot2) { create :static_slot }
+      let(:static_slot1) { create(:static_slot) }
+      let(:static_slot2) { create(:static_slot) }
       let(:member_params) do
-        { first_name: 'updated_member',
-          last_name: 'tati',
-          static_slots_attributes: { '1564': { id: static_slot1.id.to_s, _destroy: false },
-                                     '65464': { id: static_slot2.id.to_s, _destroy: false } } }
+        {first_name: 'updated_member',
+         last_name: 'tati',
+         static_slots_attributes: {'1564': {id: static_slot1.id.to_s, _destroy: false},
+                                   '65464': {id: static_slot2.id.to_s, _destroy: false}}}
       end
 
       it 'update successfully' do
