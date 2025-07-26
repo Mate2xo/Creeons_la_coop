859dc8446e5ce3fe2282b98b5e0f40400a680094
Tim <timothee.bull@gmail.com>
2025-07-26 20:30:20 +0200

    refactor(JS): scope code closer to the HTML it works on

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//
+//= link missions/_form.js
+//= link missions/_standard_quick_enrollment_form.js
