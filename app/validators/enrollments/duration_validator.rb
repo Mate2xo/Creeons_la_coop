038c4910c45d572a98e54d755583a3effd9d94f5
Tim <timothee.bull@gmail.com>
2025-08-05 14:55:15 +0200

    docs: syntax

--- app/validators/enrollments/duration_validator.rb
+++ app/validators/enrollments/duration_validator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Enrollments
-  # This validator check if the duration of the enrollment is positve.
+  # This validator checks that the duration of the enrollment is positive.
   # For a regulated Mission, checks that it has a 90 min duration.
   class DurationValidator < ActiveModel::Validator
     def validate(enrollment)
