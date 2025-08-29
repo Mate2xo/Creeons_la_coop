dd538ce9d75365090add9681891d271ef5f2aa65
Tim <timothee.bull@gmail.com>
2025-08-29 16:44:28 +0200

    test(locales): test all available locales for shared examples

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'a view without missing translations' do
-  context 'with the :fr locale' do
-    it 'has no missing keys' do
-      I18n.with_locale(:fr) { expect { render }.not_to raise_error }
+  I18n.available_locales do |locale|
+    it "has no missing keys for the #{locale} locale" do
+      I18n.with_locale(locale) { expect { render }.not_to raise_error }
     end
   end
 end
 
 RSpec.shared_examples 'a model without missing validation error translations' do |_parameter|
-  it 'has no missing keys' do
-    resource.valid?
+  I18n.available_locales.each do |locale|
+    it "has no missing keys for the #{locale} locale" do
+      resource.valid?
 
-    I18n.available_locales.each do |locale|
       I18n.with_locale(locale) do
         expect(resource.errors.full_messages).not_to include(/translation missing/i)
       end
