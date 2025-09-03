72c71aabc499854cebc26a54c05c65856dc48a5a
Tim <timothee.bull@gmail.com>
2025-09-03 15:37:09 +0200

    fix(recurrence): use time zone to avoid miscalculations

--- app/services/recurrent_missions.rb
+++ app/services/recurrent_missions.rb
@@ -18,7 +18,7 @@ class RecurrentMissions
     recurrence_end = mission_template.recurrence_end_date.to_date
 
     return t(:select_recurrence_type_and_end) if recurrence_rule.empty? || recurrence_end.nil?
-    return t(:recurrence_end_must_not_be_past) if recurrence_end < Date.today
+    return t(:recurrence_end_must_not_be_past) if recurrence_end < Date.current
     return t(:impossible_recurrence) unless RecurringSelect.is_valid_rule? recurrence_rule
 
     true
