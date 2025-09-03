bc624f2d00b16d890223fbc844e07184c95e59bd
Tim <timothee.bull@gmail.com>
2025-09-03 15:45:41 +0200

    fix(Mission new form): keep state when invalidated

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -78,7 +78,7 @@ class MissionsController < ApplicationController
       flash[:error] = translate 'activerecord.errors.messages.creation_fail',
                                 model: Mission.model_name.human
       flash[:error] << " #{mission.errors.full_messages.join(', ')}"
-      redirect_to new_mission_path
+      render :new
     end
   end
 
