5da5603bac5c02b641bb13c2bbf721f8145d3c8b
Tim <timothee.bull@gmail.com>
2025-09-03 15:46:12 +0200

    feat(Calendar): show slots from 6am

--- app/assets/javascripts/fullCalendar.js
+++ app/assets/javascripts/fullCalendar.js
@@ -17,7 +17,7 @@ document.addEventListener('turbolinks:load', () => {
     firstDay: 1,
     locale: 'fr',
     timeZone: 'UTC', // override browser default (since the shop is local)
-    minTime: '08:00:00',
+    minTime: '06:00:00',
     maxTime: '23:00:00',
     buttonText: {
       today: "Aujourd'hui",
