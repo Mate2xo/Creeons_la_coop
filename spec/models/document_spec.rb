e623f70bc826819ede9797fc664e52c4e59eca30
Tim <timothee.bull@gmail.com>
2025-09-05 10:41:05 +0200

    chore: lint

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Document, type: :model do
+RSpec.describe Document do
   context 'with a file of a whitelisted content_type' do
-    subject { Document.new(file: fixture_file_upload('test.txt')) }
+    subject { described_class.new(file: fixture_file_upload('test.txt')) }
 
     it { is_expected.to be_valid }
   end
 
   context 'with a file of a non-whitelisted content_type' do
-    subject { Document.new(file: fixture_file_upload('fixture.json')) }
+    subject { described_class.new(file: fixture_file_upload('fixture.json')) }
 
     it { is_expected.not_to be_valid }
   end
 
   context 'without any attached file' do
-    subject { Document.new(file: nil) }
+    subject { described_class.new(file: nil) }
 
     it { is_expected.not_to be_valid }
   end
