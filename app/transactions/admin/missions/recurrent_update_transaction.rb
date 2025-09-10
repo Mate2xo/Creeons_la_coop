8bc2b34682d6b0caeec0a624d7d1d125374533b1
Tim <timothee.bull@gmail.com>
2025-09-10 17:02:43 +0200

    feat(recurrent missions): ensure only recurrent missions are updated

--- app/transactions/admin/missions/recurrent_update_transaction.rb
+++ app/transactions/admin/missions/recurrent_update_transaction.rb
@@ -34,10 +34,11 @@ module Admin
       # @return [Hash]
       def get_missions_to_update(params:, old_mission:)
         recurrent_change = params[:recurrent_change] == '1'
-        missions = if recurrent_change
+        missions = if recurrent_change && old_mission.recurrent?
                      Mission.where('start_date >= :mission_start_date AND genre = :mission_genre',
                                    mission_start_date: old_mission.start_date,
-                                   mission_genre: Mission.genres[old_mission.genre])
+                                   mission_genre: Mission.genres[old_mission.genre],
+                                   recurrent: true)
                             .select do |current_mission|
                        current_mission.start_date.strftime('%R%u') == old_mission.start_date.strftime('%R%u')
                      end
--- spec/transactions/admin/missions/recurrent_update_transaction_spec.rb
+++ spec/transactions/admin/missions/recurrent_update_transaction_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Admin::Missions::RecurrentUpdateTransaction do
       ).permit!
     end
 
-    let(:mission) { create(:mission) }
+    let(:mission) { create(:mission, recurrent: true) }
 
     it 'does not update futures missions that match the same week day, hour, and genre' do
       other_missions = create_future_missions_with_matching_time_and_weekday(mission)
@@ -67,7 +67,7 @@ RSpec.describe Admin::Missions::RecurrentUpdateTransaction do
       ).permit!
     end
 
-    let(:mission) { create(:mission) }
+    let(:mission) { create(:mission, recurrent: true) }
 
     it 'updates futures missions that match the same week day, hour, and genre' do
       other_missions = create_future_missions_with_matching_time_and_weekday(mission)
@@ -105,6 +105,39 @@ RSpec.describe Admin::Missions::RecurrentUpdateTransaction do
 
       other_missions.each(&:reload)
       expect(other_missions.pluck(:due_date)).to eq original_due_dates
+    end
+  end
+
+  context 'with a truthy :recurrent_change param and a non-recurrent Mission' do
+    let(:params) do
+      ActionController::Parameters.new(
+        {
+          'name' => 'updated_mission',
+          'start_date(1i)' => mission.start_date.year.to_s,
+          'start_date(2i)' => mission.start_date.month.to_s,
+          'start_date(3i)' => mission.start_date.day.to_s,
+          'start_date(4i)' => (mission.start_date.hour + 3).to_s,
+          'start_date(5i)' => mission.start_date.min.to_s,
+          'due_date(1i)' => mission.due_date.year.to_s,
+          'due_date(2i)' => mission.due_date.month.to_s,
+          'due_date(3i)' => mission.due_date.day.to_s,
+          'due_date(4i)' => (mission.due_date.hour + 3).to_s,
+          'due_date(5i)' => mission.due_date.min.to_s,
+          'recurrent_change' => '1'
+        }
+      ).permit!
+    end
+
+    let(:mission) { create(:mission, recurrent: false) }
+
+    it 'does not update futures missions that match the same week day, hour, and genre' do
+      other_missions = create_future_missions_with_matching_time_and_weekday(mission)
+
+      transaction
+
+      other_missions.each do |mission|
+        expect(mission.reload.name).not_to eq 'updated_mission'
+      end
     end
   end
 end
